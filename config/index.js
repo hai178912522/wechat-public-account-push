46b518b8511e0027f7a1b7387a3ef0ece9a0a911
hai178912522 <59815858+hai178912522@users.noreply.github.com>
2022-08-27 13:16:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,38 +1,27 @@
-export const config = {
-    // 公众号配置
-    // 公众号appId
-    appId: "",
-    // 公众号appSecret
-    appSecret: "",
-    // 模板消息id
-    templateId: "",
-    // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
-     
-    // 信息配置
-    // 所在省份
-    province: "广东",
-    // 所在城市
-    city: "惠州",
-    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
-    ],
-    // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
-    }
+{
+# 公众号配置
+# 公众号appId
+"app_id": "wxa99f70ca93e00847",
+# 公众号appSecret
+"app_secret": "72ebbb15c7e12e38b8b38d3f01257351",
+# 模板消息id
+"template_id": "ovYa9DrIw5GZLWtQ8TiRY4q1OVFQRlylq9e9KZHd0qE",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+"user": ["ojPYT50XdTpF7gRM_h3yNMrQKFzU","ojPYT524L8_hlYHJjysxbUlQvPPw"],
 
-// {{date.DATA}}  
-// 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
-// {{birthday_message.DATA}}
-
-// {{note_en.DATA}}  
-// {{note_ch.DATA}}
+# 信息配置
+# 和风天气apikey
+"weather_key": "dc3b1f4aee174d8a821eefd8828e9385",
+# 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
+"region": "浦口区",
+# 生日1，修改名字为对应需要显示的名字，如果生日为农历，在最前面加上r即可
+"birthday1": {"name": "大宝贝", "birthday": "1996-10-12"},
+# 生日2
+"birthday2": {"name": "臭老公", "birthday": "r1995-03-07"},
+# 在一起的日子，格式同上，暂不不支持农历
+"love_date": "2019-11-01",
+# 金句中文，如果设置了，则会显示这里的，如果为空，默认会读取金山的每日金句
+"note_ch": "我爱你至天荒地老",
+# 金句英文
+"note_en": "i love you to the moon and back"
+}
